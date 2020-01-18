98ba9f7870860d35324c8f8da2566387ed5f4ce4
GooGee <mou-gj@live.com>
2020-01-18 11:13:18 +0800

    + Bar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="app">
+        <Bar />
         <Home />
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Bar from './components/Bar.vue'
 import Home from './components/Home.vue'
 
 export default Vue.extend({
     name: 'app',
     components: {
+      Bar,
       Home,
     },
 })
---
+++ src/components/Bar.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-navbar-brand href="#">Ark Loot</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item @click="bus.tab.show('home')" href="#">Home</b-nav-item>
+                <b-nav-item @click="bus.tab.show('crate')" href="#">Crate</b-nav-item>
+                <b-nav-item @click="bus.tab.show('set')" href="#">ItemSet</b-nav-item>
+                <b-nav-item @click="bus.tab.show('item')" href="#">Item</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item href="https://github.com/GooGee/Loot">GitHub</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import bus from '../model/bus'
+
+export default Vue.extend({
+    name: 'Bar',
+    data() {
+        return {
+            bus,
+        }
+    },
+})
+</script>
