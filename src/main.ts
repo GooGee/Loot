9cdef9d36129037bd7725360a8f252a4c011e917
GooGee <mou-gj@live.com>
2020-01-18 11:02:52 +0800

    + dependency bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
