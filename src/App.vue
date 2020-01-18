d36935922af920a596c6fbe1ad9e2bc1bd92b9e5
GooGee <mou-gj@live.com>
2020-01-18 11:06:04 +0800

    - HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div id="app">
+        <Home />
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
+import Home from './components/Home.vue'
 
 export default Vue.extend({
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-});
+    name: 'app',
+    components: {
+      Home,
+    },
+})
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
