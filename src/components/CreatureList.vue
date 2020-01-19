80d1c30a28d503584c45f3dfe2b38830f60fb01f
GooGee <mou-gj@live.com>
2020-01-19 16:27:50 +0800

    + CreatureList.vue

---
+++ src/components/CreatureList.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-table striped hover caption-top :items="manager.list" :fields="fields">
+        <template v-slot:table-caption>
+            <select>
+                <option value="">All</option>
+            </select>
+        </template>
+    </b-table>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import bus from '../model/bus'
+
+export default Vue.extend({
+    name: 'CreatureList',
+    data() {
+        return {
+            bus,
+            manager: bus.CreatureManager,
+            fields: [
+                {
+                    key: 'name',
+                    label: 'Name',
+                },
+            ],
+        }
+    },
+    created() {},
+    computed: {},
+    methods: {},
+})
+</script>
