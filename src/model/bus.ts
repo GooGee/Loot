36fc4012606e4be4158033dc7657160c6e204246
GooGee <mou-gj@live.com>
2020-01-19 16:28:57 +0800

    + loadData in bus.ts

--- src/model/bus.ts
+++ src/model/bus.ts
@@ -1,5 +1,30 @@
 import Tab from './Tab'
+import Adaptor from './Adaptor/Adaptor'
+import CrateManager from './Ark/CrateManager'
+import CreatureManager from './Ark/CreatureManager'
+import EngramManager from './Ark/EngramManager'
 
-export default {
-    tab: new Tab,
+export class Bus {
+    tab = new Tab
+    private data: Object | null = null
+    readonly CrateManager = new CrateManager
+    readonly CreatureManager = new CreatureManager
+    readonly EngramManager = new EngramManager
+
+    constructor() {
+        this.loadData()
+    }
+
+    loadData() {
+        fetch('data.json').then(response => {
+            response.json().then(json => {
+                if (json) {
+                    this.data = json
+                    Adaptor.run(this.data as any, this)
+                }
+            })
+        }).catch(error => console.error(error))
+    }
 }
+
+export default new Bus
