0255ac843b9b4f1d51641b528e97b2a7bfc2c675
GooGee <mou-gj@live.com>
2020-01-21 14:08:12 +0800

    + deploy in Engram.ts

--- src/model/Ark/Engram.ts
+++ src/model/Ark/Engram.ts
@@ -4,4 +4,8 @@ export default class Engram extends ArkItem {
     canBeBluePrint: boolean = false
 
     Multiplier: number = 1
+
+    deploy() {
+        return `HarvestResourceItemAmountClassMultipliers=(ClassName="${this.name}",Multiplier=${this.Multiplier})`
+    }
 }
