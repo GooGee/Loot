fba7b65981a051b0913ad137776f3cc689ad8351
GooGee <mou-gj@live.com>
2020-01-21 10:19:51 +0800

    + CSS active in Bar.vue

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -7,13 +7,17 @@
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item @click="bus.tab.show('Home')" href="#">Home</b-nav-item>
-                    <b-nav-item @click="bus.tab.show('Ark')" href="#">Ark</b-nav-item>
+                    <b-nav-item :class="{ active: bus.tab.is('Home') }" @click="bus.tab.show('Home')" href="#">
+                        Home
+                    </b-nav-item>
+                    <b-nav-item :class="{ active: bus.tab.is('Ark') }" @click="bus.tab.show('Ark')" href="#">
+                        Ark
+                    </b-nav-item>
                 </b-navbar-nav>
 
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item href="https://github.com/GooGee/Loot">GitHub</b-nav-item>
+                    <b-nav-item href="https://github.com/GooGee/Loot" target="_blank">GitHub</b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </div>
