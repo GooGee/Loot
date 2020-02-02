a88e4c742d53e5c6d8a774c79ffe38739509f9db
GooGee <mou-gj@live.com>
2020-02-02 17:29:46 +0800

    - Entry

--- src/model/Ark/EntryManager.ts
+++
@@ -1,6 +0,0 @@
-import Entry from './Entry'
-import UniqueList from '../Entity/UniqueList'
-
-export default class EntryManager extends UniqueList<Entry> {
-
-}
--- src/model/Ark/ItemSetManager.ts
+++
@@ -1,6 +0,0 @@
-import UniqueList from '../Entity/UniqueList'
-import ItemSet from './ItemSet'
-
-export default class ItemSetManager extends UniqueList<ItemSet> {
-
-}
