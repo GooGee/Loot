f1ba5e701670756bb7158cfecc7bb879d8c871ff
GooGee <mou-gj@live.com>
2020-02-02 16:45:31 +0800

    * bus.ts

--- src/model/bus.ts
+++ src/model/bus.ts
@@ -5,13 +5,13 @@ import Game from './Ark/Game'
 export class Bus {
     tab = new Tab
     private data: Object | null = null
-    readonly game =new Game
+    readonly game = new Game
 
     constructor() {
-        this.loadData()
+        this.load()
     }
 
-    loadData() {
+    load() {
         fetch('data.json').then(response => {
             response.json().then(json => {
                 if (json) {
