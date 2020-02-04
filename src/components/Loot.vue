67f76c4aaf6f9ac30f4812ab88fe741e59840d09
GooGee <mou-gj@live.com>
2020-02-04 11:09:35 +0800

    + row in Loot.vue

--- src/components/Loot.vue
+++ src/components/Loot.vue
@@ -1,45 +1,52 @@
 <template>
-    <b-table-simple hover caption-top>
-        <caption>
-            <h1>{{ bus.loot.label }}</h1>
+    <div class="row">
+        <div class="col-8">
+            <b-table-simple hover caption-top>
+                <caption>
+                    <h1>{{ bus.loot.label }}</h1>
+                </caption>
+                <b-thead>
+                    <b-tr>
+                        <b-th width="30%">Name</b-th>
+                        <b-th>Weight</b-th>
+                        <b-th width="40%">Item List</b-th>
+                    </b-tr>
+                </b-thead>
+                <b-tbody>
+                    <b-tr v-for="set in manager.list" :key="set.name">
+                        <b-td>
+                            <b-button @click="remove(set)" variant="outline-danger"> - {{ set.label }} </b-button>
+                        </b-td>
+                        <b-td>
+                            <b-form-input v-model.number="set.SetWeight" type="number" min="1" step="1"></b-form-input>
+                        </b-td>
+                        <b-td>
+                            <ul>
+                                <li v-for="item in set.EntryManager.list" :key="item.name">{{ item.label }}</li>
+                            </ul>
+                        </b-td>
+                    </b-tr>
+                </b-tbody>
+                <b-tfoot>
+                    <b-tr>
+                        <b-td></b-td>
+                        <b-td>
+                            <b-button @click="setWeight" variant="outline-primary"> Set </b-button>
+                        </b-td>
+                        <b-td></b-td>
+                    </b-tr>
+                </b-tfoot>
+            </b-table-simple>
+        </div>
+        <div class="col-4">
+            <h4>ItemSet</h4>
             <ul>
                 <li v-for="set in bus.game.ItemSetManager.list" :key="set.name">
                     <b-button @click="add(set)" variant="outline-primary"> + {{ set.label }} </b-button>
                 </li>
             </ul>
-        </caption>
-        <b-thead>
-            <b-tr>
-                <b-th width="30%">Name</b-th>
-                <b-th>Weight</b-th>
-                <b-th width="40%">Item List</b-th>
-            </b-tr>
-        </b-thead>
-        <b-tbody>
-            <b-tr v-for="set in manager.list" :key="set.name">
-                <b-td>
-                    <b-button @click="remove(set)" variant="outline-danger"> - {{ set.label }} </b-button>
-                </b-td>
-                <b-td>
-                    <b-form-input v-model.number="set.SetWeight" type="number" min="1" step="1"></b-form-input>
-                </b-td>
-                <b-td>
-                    <ul>
-                        <li v-for="item in set.EntryManager.list" :key="item.name">{{ item.label }}</li>
-                    </ul>
-                </b-td>
-            </b-tr>
-        </b-tbody>
-        <b-tfoot>
-            <b-tr>
-                <b-td></b-td>
-                <b-td>
-                    <b-button @click="setWeight" variant="outline-primary"> Set </b-button>
-                </b-td>
-                <b-td></b-td>
-            </b-tr>
-        </b-tfoot>
-    </b-table-simple>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
