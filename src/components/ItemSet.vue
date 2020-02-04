0d657d270531b23fcf51a49cd2f10ee591910571
GooGee <mou-gj@live.com>
2020-02-04 11:05:18 +0800

    + row in ItemSet.vue

--- src/components/ItemSet.vue
+++ src/components/ItemSet.vue
@@ -1,65 +1,77 @@
 <template>
-    <b-table-simple hover caption-top>
-        <caption>
-            <h1>{{ bus.set.label }}</h1>
-            <EngramFilter :manager="bus.game.EngramManager"></EngramFilter>
-            <div>
-                <b-button @click="addAll" variant="outline-success"> + All </b-button>
-            </div>
+    <div class="row">
+        <div class="col-8">
+            <b-table-simple hover caption-top>
+                <caption>
+                    <h1>{{ bus.set.label }}</h1>
+                </caption>
+                <b-thead>
+                    <b-tr>
+                        <b-th width="40%">Name</b-th>
+                        <b-th>Weight</b-th>
+                        <b-th>Amount</b-th>
+                        <b-th>Blueprint Chance</b-th>
+                    </b-tr>
+                </b-thead>
+                <b-tbody>
+                    <b-tr v-for="item in manager.list" :key="item.name">
+                        <b-td>
+                            <b-button @click="remove(item)" variant="outline-danger"> - {{ item.label }} </b-button>
+                        </b-td>
+                        <b-td>
+                            <b-form-input
+                                v-model.number="item.EntryWeight"
+                                type="number"
+                                min="1"
+                                step="1"
+                            ></b-form-input>
+                        </b-td>
+                        <b-td>
+                            <b-form-input v-model.number="item.quantity" type="number" min="1" step="1"></b-form-input>
+                        </b-td>
+                        <b-td>
+                            <b-form-input
+                                v-if="item.canBeBluePrint"
+                                v-model.number="item.ChanceToBeBlueprintOverride"
+                                type="number"
+                                min="0"
+                                max="1"
+                                step="0.1"
+                            ></b-form-input>
+                        </b-td>
+                    </b-tr>
+                </b-tbody>
+                <b-tfoot>
+                    <b-tr>
+                        <b-td>
+                            <b-button @click="removeAll" variant="outline-danger"> - All </b-button>
+                        </b-td>
+                        <b-td>
+                            <b-button @click="setWeight" variant="outline-primary"> Set </b-button>
+                        </b-td>
+                        <b-td>
+                            <b-button @click="setQuantity" variant="outline-primary"> Set </b-button>
+                        </b-td>
+                        <b-td>
+                            <b-button @click="setChance" variant="outline-primary"> Set </b-button>
+                        </b-td>
+                    </b-tr>
+                </b-tfoot>
+            </b-table-simple>
+        </div>
+        <div class="col-4">
+            <h4>Item</h4>
+            <EngramFilter :manager="bus.game.EngramManager" :inline="false"></EngramFilter>
             <ul>
+                <li>
+                    <b-button @click="addAll" variant="outline-success"> + All </b-button>
+                </li>
                 <li v-for="item in bus.game.EngramManager.filtered" :key="item.name">
                     <b-button @click="add(item)" variant="outline-primary"> + {{ item.label }} </b-button>
                 </li>
             </ul>
-        </caption>
-        <b-thead>
-            <b-tr>
-                <b-th width="30%">Name</b-th>
-                <b-th>Weight</b-th>
-                <b-th>Amount</b-th>
-                <b-th>Blueprint Chance</b-th>
-            </b-tr>
-        </b-thead>
-        <b-tbody>
-            <b-tr v-for="item in manager.list" :key="item.name">
-                <b-td>
-                    <b-button @click="remove(item)" variant="outline-danger"> - {{ item.label }} </b-button>
-                </b-td>
-                <b-td>
-                    <b-form-input v-model.number="item.EntryWeight" type="number" min="1" step="1"></b-form-input>
-                </b-td>
-                <b-td>
-                    <b-form-input v-model.number="item.quantity" type="number" min="1" step="1"></b-form-input>
-                </b-td>
-                <b-td>
-                    <b-form-input
-                        v-if="item.canBeBluePrint"
-                        v-model.number="item.ChanceToBeBlueprintOverride"
-                        type="number"
-                        min="0"
-                        max="1"
-                        step="0.1"
-                    ></b-form-input>
-                </b-td>
-            </b-tr>
-        </b-tbody>
-        <b-tfoot>
-            <b-tr>
-                <b-td>
-                    <b-button @click="removeAll" variant="outline-danger"> - All </b-button>
-                </b-td>
-                <b-td>
-                    <b-button @click="setWeight" variant="outline-primary"> Set </b-button>
-                </b-td>
-                <b-td>
-                    <b-button @click="setQuantity" variant="outline-primary"> Set </b-button>
-                </b-td>
-                <b-td>
-                    <b-button @click="setChance" variant="outline-primary"> Set </b-button>
-                </b-td>
-            </b-tr>
-        </b-tfoot>
-    </b-table-simple>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
