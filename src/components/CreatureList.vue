cdd4e8730446cc5dae420310b25c9be671c86f91
GooGee <mou-gj@live.com>
2020-02-05 16:12:41 +0800

    + modal in CreatureList.vue

--- src/components/CreatureList.vue
+++ src/components/CreatureList.vue
@@ -1,6 +1,6 @@
 <template>
-    <b-table-simple hover caption-top>
-        <caption>
+    <div class="row">
+        <div class="col-4">
             <h1>Replace Creature</h1>
             <p>"NPCReplacements"</p>
             <div>
@@ -11,31 +11,50 @@
                     text-field="name"
                 ></b-form-select>
             </div>
-        </caption>
-        <b-thead>
-            <b-tr>
-                <b-th width="30%">Name</b-th>
-                <b-th>To</b-th>
-            </b-tr>
-        </b-thead>
-        <b-tbody>
-            <b-tr v-for="item in manager.filtered" :key="item.name">
-                <b-td>
-                    <b-form-checkbox v-model="item.included">{{ item.label }}</b-form-checkbox>
-                </b-td>
-                <b-td>
-                    <b-form-select
-                        v-if="selected == item"
-                        v-model="item.ToClassName"
-                        :options="manager.filtered"
-                        value-field="name"
-                        text-field="label"
-                    ></b-form-select>
-                    <b-button v-else @click="selected = item" variant="outline-primary"> {{ item.ToClassName || '+' }} </b-button>
-                </b-td>
-            </b-tr>
-        </b-tbody>
-    </b-table-simple>
+            <br />
+        </div>
+        <div class="col-8"></div>
+
+        <div class="col-12">
+            <b-table-simple hover caption-top>
+                <caption>
+                    <b-modal id="creature-modal" hide-footer title="Select a Creature">
+                        <div v-if="selected">
+                            <b-form-select
+                                v-model="selected.ToClassName"
+                                :options="manager.filtered"
+                                value-field="name"
+                                text-field="label"
+                            ></b-form-select>
+                        </div>
+                        <div>
+                            <b-button class="mt-3" variant="outline-danger" @click="$bvModal.hide('creature-modal')">
+                                Close
+                            </b-button>
+                        </div>
+                    </b-modal>
+                </caption>
+                <b-thead>
+                    <b-tr>
+                        <b-th width="50%">Name</b-th>
+                        <b-th>Replace</b-th>
+                    </b-tr>
+                </b-thead>
+                <b-tbody>
+                    <b-tr v-for="item in manager.filtered" :key="item.name">
+                        <b-td>
+                            <b-form-checkbox v-model="item.included">{{ item.label }}</b-form-checkbox>
+                        </b-td>
+                        <b-td>
+                            <b-button @click="show(item)" variant="outline-primary">
+                                {{ item.ToClassName || '+' }}
+                            </b-button>
+                        </b-td>
+                    </b-tr>
+                </b-tbody>
+            </b-table-simple>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -53,6 +72,11 @@ export default Vue.extend({
     },
     created() {},
     computed: {},
-    methods: {},
+    methods: {
+        show(item) {
+            this.selected = item
+            this.$bvModal.show('creature-modal')
+        },
+    },
 })
 </script>
